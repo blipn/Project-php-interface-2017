a97e22359f6f206447d414f9e3729df3416d9bcf
blipn <oblipn@gmail.com>
2017-04-27 11:21:30 +0200

    interface ressouce

---
+++ php-interface-2017/web/js/scripts.js
@@ -0,0 +1,27 @@
+function openTab(evt, tab) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = "iconTabs tablinks";
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tab).style.display = "block";
+
+    if(typeof evt === "string"){
+        document.getElementById(evt).className = "iconTabsSelected tablinks";
+    }else {
+        evt.currentTarget.className = "iconTabsSelected tablinks";
+    }
+}
+
+openTab("menuDashboard", 'dashboard'); // on charge la tab 1
