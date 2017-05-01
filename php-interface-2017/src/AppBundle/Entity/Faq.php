a53754130703c892dcbf3492c7845af6ed37d9b1
Lucas <biblion84@free.fr>
2017-05-01 11:44:11 +0200

    Ajouts des fichiers de BDD faq et sav

---
+++ php-interface-2017/src/AppBundle/Entity/Faq.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Faq
+ *
+ * @ORM\Table(name="faq")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FaqRepository")
+ */
+class Faq
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="categorie", type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="question", type="string", length=1024)
+     */
+    private $question;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reponse", type="string", length=4096)
+     */
+    private $reponse;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param string $categorie
+     *
+     * @return Faq
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return string
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * Set question
+     *
+     * @param string $question
+     *
+     * @return Faq
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set reponse
+     *
+     * @param string $reponse
+     *
+     * @return Faq
+     */
+    public function setReponse($reponse)
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+
+    /**
+     * Get reponse
+     *
+     * @return string
+     */
+    public function getReponse()
+    {
+        return $this->reponse;
+    }
+}
---
+++ php-interface-2017/src/AppBundle/Entity/Sav.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sav
+ *
+ * @ORM\Table(name="sav")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SavRepository")
+ */
+class Sav
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="etat", type="string", length=255)
+     */
+    private $etat;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="diagnostique", type="string", length=1024)
+     */
+    private $diagnostic;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Utilisateur")
+     */
+    private $utilisateur;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set etat
+     *
+     * @param string $etat
+     *
+     * @return Sav
+     */
+    public function setEtat($etat)
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    /**
+     * Get etat
+     *
+     * @return string
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Sav
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set diagnostique
+     *
+     * @param string $diagnostique
+     *
+     * @return Sav
+     */
+
+    /**
+     * Set diagnostic
+     *
+     * @param string $diagnostic
+     *
+     * @return Sav
+     */
+    public function setDiagnostic($diagnostic)
+    {
+        $this->diagnostic = $diagnostic;
+
+        return $this;
+    }
+
+    /**
+     * Get diagnostic
+     *
+     * @return string
+     */
+    public function getDiagnostic()
+    {
+        return $this->diagnostic;
+    }
+
+    /**
+     * Set utilisateur
+     *
+     * @param \AppBundle\Entity\Utilisateur $utilisateur
+     *
+     * @return Sav
+     */
+    public function setUtilisateur(\AppBundle\Entity\Utilisateur $utilisateur = null)
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get utilisateur
+     *
+     * @return \AppBundle\Entity\Utilisateur
+     */
+    public function getUtilisateur()
+    {
+        return $this->utilisateur;
+    }
+}
